68af8c38649696b2ef65686c51328bc40c183457
DaviRdaSilva <Davi.rsilva@sptech.school>
2023-03-09 19:24:24 -0300

    Atualização do codigo Arduino

--- Arduino/Sensores.ino
+++ Arduino/Sensores.ino
@@ -1,34 +1,58 @@
 
-#include "DHT.h"
-#define dht_type DHT11 
-int dht_pin = A1;
-DHT dht_1 = DHT(dht_pin, dht_type);
-const int LM35 = A0;
-float temperatura;
+#include <DHT.h>
+#include <DHT.h>
 
-void setup(){
+#define DHTPIN A1
+#define LM35PIN A0
 
-    Serial.begin(9600);
-    dht_1.begin();
+DHT dht(DHTPIN, DHT11);
+
+void setup()
+{
+
+  pinMode(DHTPIN, INPUT);
+  Serial.begin(9600);
+  dht.begin();
 
 }
 
-void loop(){
+void loop()
+{
 
-    temperatura = (float(analogRead(LM35))*5/(1023))/0.01;
-    float umidade = dht_1.readHumidity();
+    float dht11_umidade = dht.readHumidity();
+    float dht11_temperatura = dht.readTemperature();
+    float lm35_temperatura = analogRead(LM35PIN);
+    lm35_temperatura = (lm35_temperatura * 0.00488) * 100;
 
-    if (isnan(temperatura) or isnan(umidade))
+    if(isnan dht11_temperatura or isnan lm35_temperatura)
     {
-        Serial.println("Erro ao ler os Sensores");
+        Serial.println("Erro ao captar os dados");
     }
     else
     {
-        Serial.print(umidade);
-        Serial.print(" % ");
-        Serial.println(temperatura);
+        Serial.print(dht11_umidade);
+        Serial.print(";");
+        Serial.print(lm35_temperatura);
+        Serial.println(";");
     }
-
-    delay(2000);
-
+  
 }
+
+//  #define LUMIPIN A2
+//  #define CHAVPIN 7
+//  pinMode(CHAVPIN, INPUT);
+//  int chave = digitalRead(7);
+
+//  if (chave == 0)
+//  {
+//      Serial.print("1");
+//  }
+//  else
+//  {
+//      Serial.print("0");
+//  }
+
+//  float luminosidade = analogRead(LUMIPIN);
+ 
+//  Serial.print(luminosidade);
+//  Serial.print(";");
