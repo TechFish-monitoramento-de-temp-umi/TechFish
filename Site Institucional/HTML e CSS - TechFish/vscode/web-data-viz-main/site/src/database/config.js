42e6a47cc53d7985eebb2e960d9f5bac87e08dca
GustavoHMarchi <gustavo.holiveira@sptech.school>
2023-05-29 14:51:04 -0300

    Conexão das duas apis - não finalizado

--- Site Institucional/HTML e CSS - TechFish/vscode/web-data-viz-main/site/src/database/config.js
+++ Site Institucional/HTML e CSS - TechFish/vscode/web-data-viz-main/site/src/database/config.js
@@ -21,8 +21,8 @@ var sqlServerConfig = {
 var mySqlConfig = {
     host: "localhost",
     database: "techfish",
-    user: "techfish",
-    password: "teste123@",
+    user: "root",
+    password: "rootpassword",
 };
 
 function executar(instrucao) {
--- Site Institucional/HTML e CSS - TechFish/vscode/web-data-viz-main/site/src/models/medidaModel.js
+++ Site Institucional/HTML e CSS - TechFish/vscode/web-data-viz-main/site/src/models/medidaModel.js
@@ -1,6 +1,6 @@
 var database = require("../database/config");
 
-function buscarUltimasMedidas(idAquario, limite_linhas) {
+function buscarUltimasMedidas(idLeitura, limite_linhas) {
 
     instrucaoSql = ''
 
@@ -11,7 +11,7 @@ function buscarUltimasMedidas(idAquario, limite_linhas) {
                         momento,
                         FORMAT(momento, 'HH:mm:ss') as momento_grafico
                     from medida
-                    where fk_aquario = ${idAquario}
+                    where fk_aquario = ${idLeitura}
                     order by id desc`;
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
         instrucaoSql = `select 
@@ -20,7 +20,7 @@ function buscarUltimasMedidas(idAquario, limite_linhas) {
                         momento,
                         DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
                     from medida
-                    where fk_aquario = ${idAquario}
+                    where fk_aquario = ${iLeitura}
                     order by id desc limit ${limite_linhas}`;
     } else {
         console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
@@ -31,27 +31,25 @@ function buscarUltimasMedidas(idAquario, limite_linhas) {
     return database.executar(instrucaoSql);
 }
 
-function buscarMedidasEmTempoReal(idAquario) {
+function buscarMedidasEmTempoReal(idLeitura) {
 
     instrucaoSql = ''
 
     if (process.env.AMBIENTE_PROCESSO == "producao") {
-        instrucaoSql = `select top 1
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,  
-                        CONVERT(varchar, momento, 108) as momento_grafico, 
-                        fk_aquario 
-                        from medida where fk_aquario = ${idAquario} 
-                    order by id desc`;
+        instrucaoSql = `select tempDHT11 as temp1,
+        tempLM35 as temp2,
+        umidade,
+        fkSensores as sensor from leitura
+        where fkSensores = ${idLeitura}
+        order by idLeitura desc limit 1`;
 
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
-        instrucaoSql = `select 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
-                        fk_aquario 
-                        from medida where fk_aquario = ${idAquario} 
-                    order by id desc limit 1`;
+        instrucaoSql = `select tempDHT11 as temp1,
+        tempLM35 as temp2,
+        umidade,
+        fkSensores as sensor from leitura
+        where fkSensores = ${idLeitura}
+        order by idLeitura desc limit 1`;
     } else {
         console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
         return
